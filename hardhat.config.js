ab24bae329d46e103eb5a883b1e4eb4983d8eb30
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-11-14 11:43:53 +0000

    updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,13 +5,13 @@ const fs = require("fs");
 // const privateKey = fs.readFileSync("secret.txt").toString();
 const projectId = "8b7ba5517c414450a93ec7334975a7fe";
 const privatekey1 =
-  "e5521236a22b16a827161afb56a2fd5fd1c48d0b4dc3dd960a96ec8cdad914cc";
+  "";
 const privatekey2 =
-  "de3e7171e0654efd4179bc967277eeff17227b0860a8a4eb139dfc45f9adda3f";
+  "";
 const privatekey3 =
-  "0fac9610dc1eac5c9479f01804737b95fb0c7ca0b9ab9c2d92d9ca00c252058b";
+  "";
 const privatekey4 =
-  "6b2eea68a372d42f933f6808f01313db0bdde58660284102e1142f88423c3536";
+  "";
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
